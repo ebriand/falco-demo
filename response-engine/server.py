3ec0210840abd52abddbe6cd96c1c7c37a091bad
Eric <eric.briand@gmail.com>
2020-11-12 15:50:05 +0100

    :sparkles: add kube client

--- response-engine/server.py
+++ response-engine/server.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""
-Very simple HTTP server in python for logging requests
-Usage::
-    ./server.py [<port>]
-"""
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+import json
+from kubernetes import client, config
+
+config.load_incluster_config()
+v1 = client.CoreV1Api()
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
@@ -13,17 +13,15 @@ class S(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
-
+                str(self.path), str(self.headers), post_data)
+        data = json.loads(post_data)
+        namespace = data["output_fields"]["k8s.ns.name"]
+        pod = data["output_fields"]["k8s.pod.name"]
+        v1.delete_namespaced_pod(namespace, pod)
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
