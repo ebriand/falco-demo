af13291cea2f4b4af298c2a8020a8a20490b1ea5
Pierre-Yves Aillet <pyaillet@gmail.com>
2020-11-24 22:05:22 +0100

    :memo: full scenario

--- deploy-falco.sh
+++ deploy-falco.sh
@@ -4,3 +4,5 @@ helm repo add falcosecurity https://falcosecurity.github.io/charts
 helm repo update
 
 helm install -f falco-values.yaml falco falcosecurity/falco
+
+kubectl apply -f ./response-engine/k8s/response-engine.yaml
