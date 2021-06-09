e21619f9136c87ca8a963af267ed4d994ea50254
Pierre-Yves Aillet <pyaillet@gmail.com>
2021-06-09 22:52:28 +0200

    automate demo setup

--- deploy-falco.sh
+++ deploy-falco.sh
@@ -11,4 +11,11 @@ helm repo update
 
 helm install -f "falco-values.${TARGET}.yaml" falco falcosecurity/falco
 
+kubectl apply -f falco-cm.yaml
+kubectl delete po -l app=falco
+
+if [[ $TARGET == "gke" ]]; then
+  kubectl patch svc falco-falcosidekick-ui -p '{"spec": {"type": "LoadBalancer"}}'
+fi
+
 kubectl apply -f ./response-engine/k8s/response-engine.yaml
