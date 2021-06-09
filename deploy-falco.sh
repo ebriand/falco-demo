ece88896ad72e88d4b5e450e4e71043d413f64b6
Pierre-Yves Aillet <pyaillet@gmail.com>
2021-06-09 22:08:40 +0200

    handle gke and minikube targets

--- deploy-falco.sh
+++ deploy-falco.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+if [[ $1 == "gke" ]]; then
+  TARGET=gke
+else
+  TARGET=minikube
+fi
 
 helm repo add falcosecurity https://falcosecurity.github.io/charts
 helm repo update
 
-helm install -f falco-values.yaml falco falcosecurity/falco
+helm install -f "falco-values.${TARGET}.yaml" falco falcosecurity/falco
 
 kubectl apply -f ./response-engine/k8s/response-engine.yaml
